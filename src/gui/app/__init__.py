dbd14664b10d3a797a94cbfde275bb9eeda1041d
ChenXu <chenxuhuang27@gmail.com>
2026-05-10 23:59:46 +0800

    fix(build): import tools src.gui.app module after nuitka compilation

--- src/gui/app/__init__.py
+++ src/gui/app/__init__.py
@@ -2,6 +2,10 @@
 
 __all__ = ["GUI_TOOLS"]
 
+from . import regex_replacer
+from . import table_grapher
+from . import data_generator
+
 GUI_TOOLS = [
     {"id": "regex_replacer", "icon": "📝", "color": "#4F8EF7", "module": ".app.regex_replacer"},
     {"id": "table_grapher", "icon": "🎨", "color": "#F76B4F", "module": ".app.table_grapher"},
